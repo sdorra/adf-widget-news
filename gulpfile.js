806789c2bb08830c57d792545beddf78dbcb5213
Sebastian Sdorra <sebastian.sdorra@triology.de>
2017-07-24 10:32:18 +0200

    added feedparser endpoint for testing

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,11 @@ var jsReporter = require('jshint-stylish');
 var annotateAdfPlugin = require('ng-annotate-adf-plugin');
 var pkg = require('./package.json');
 
+var url = require('url');
+var queryString = require('query-string');
+var request = require('request');
+var FeedParser = require('feedparser');
+
 var annotateOptions = {
   plugin: [
     annotateAdfPlugin
@@ -63,10 +68,100 @@ gulp.task('watch', function(){
 });
 
 gulp.task('serve', ['watch', 'sample'], function(){
+  var feed = function(req, resp, next) {
+    var rawUrl = req.originalUrl;
+    if (rawUrl.indexOf("/feed") === 0) {
+      var parsedUrl = url.parse(rawUrl);
+      var queryParams = queryString.parse(parsedUrl.query);
+
+      var feedUrl = queryParams.url;
+
+      var feedRequest = request(feedUrl);
+
+      var feedparser = new FeedParser();
+      var feedEntries = [];
+      var feedDescription = '';
+      var feedTitle = '';
+      var feedLink = '';
+      var feedError;
+
+
+      // error when server request failed
+      feedRequest.on('error', function (error) {
+        resp.writeHead(400, error.toString());
+        resp.end();
+      });
+
+      // open a stream after a valid server response
+      feedRequest.on('response', function () {
+        if (resp.statusCode !== 200) {
+          this.emit('error', new Error('Bad status code'));
+        }
+        var stream = this; // `this` is `req`, which is a stream
+        stream.pipe(feedparser);
+      });
+
+      // error when feed url could not be parsed
+      feedparser.on('error', function (error) {
+        feedError = error;
+      });
+
+      // read all feed information and save them
+      feedparser.on('readable', function () {
+        var stream = this; // `this` is `feedparser`, which is a stream
+        var item;
+        while (item = stream.read()) {
+          feedDescription = item.meta.description;
+          feedTitle = item.meta.title;
+          feedLink = item.meta.link;
+          feedEntries.push({
+            title: item.title,
+            link: item.link,
+            contentSnippet: item.summary,
+            author: item.author,
+            pubDate: item.date
+          });
+        }
+      });
+
+      // return feed information - number of Entries configured by request
+      feedparser.on('end', function () {
+        if (feedError) {
+          resp.writeHead(400, error.toString());
+          resp.end();
+        } else {
+          var numberOfEntries = queryParams.num;
+          feedEntries = feedEntries.slice(0, numberOfEntries);
+
+          var feed = {feed: {entries: feedEntries, title: feedTitle, description: feedDescription, link: feedLink}};
+
+          var callbackName = queryParams.callback;
+          if (!callbackName) {
+            callbackName = 'jsonp_callback';
+          }
+
+          resp.writeHead(200);
+          resp.write('/**/ typeof ' + callbackName + ' === "function" && ');
+          resp.write(callbackName + '(');
+          resp.write(JSON.stringify(feed));
+          resp.write(');');
+
+          resp.end();
+        }
+      });
+
+    } else {
+      next();
+    }
+  };
+
   connect.server({
     root: ['.tmp/dist', '.'],
     livereload: true,
-    port: 9002
+    port: 9002,
+    middleware: function(connect, opt) {
+      return [feed];
+    }
   });
 });
 
